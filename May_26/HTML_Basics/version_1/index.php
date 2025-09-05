69cbc24310fe3e925aa6b06d49704da462e5f195
Abdallah.Baffa <abdallah92713@gmail.com>
2025-09-05 10:37:13 +0100

    Hi, hello. Started a HTML basics folder in May 26, for me!

---
+++ May_26/HTML_Basics/version_1/index.php
@@ -0,0 +1,17 @@
+<?php // This opens the PHP code section.
+
+echo "<html>"; # Opening for HTML.
+echo "<head>"; # The head tag is above the body tag.
+
+echo "<title> Abdallah </title>"; # This is my title for index.
+
+echo "</head>";
+
+echo "<body>"; # This is the body tag, and where most of the content is displayed.
+
+echo "<h1> Hi. </h1>"; # This is a heading, it says Hello.
+
+echo "</body>";
+echo "</html>"; # Closing for HTML.
+
+?>
