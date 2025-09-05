7fcfcafef7bfde262c0262ac8bc157de1c1b9b11
Abdallah.Baffa <abdallah92713@gmail.com>
2025-09-05 13:06:24 +0100

    Hi, hello. Started a HTML basics folder in May 26, for me!

--- May_26/HTML_Basics/version_1/index.php
+++ May_26/HTML_Basics/version_1/index.php
@@ -5,31 +5,43 @@ echo "<!DOCTYPE html>";
 echo "<html>"; # Opening for HTML.
 
 
-    echo "<head>"; # The head tag is above the body tag.
-
-    echo "<title> Abdallah. </title>"; # This is my title for index.
+    echo "<head><title>Abdallah Occupational Specialism</title></head>"; # The head tag is above the body tag. This is my title for index.
 
     echo "</head>"; # Closing the head.
 
     echo "<body>"; # This is the body tag, and where most of the content is displayed.
 
-    echo "<h1> Hi. </h1>"; # This is a heading, it says Hello.
+    echo "<h1>Hi.</h1>"; # This is a heading, it says Hello.
 
-    echo "<p> So, they say every cloud has a silver lining.</p>"; # This is the paragraph tag. It makes paragraphs.
+    echo "<p>So, they say every cloud has a silver lining.</p>"; # This is the paragraph tag. It makes paragraphs.
 
-    echo "<hr> Clouds are high in the air. </hr>"; # This is the horizontal rule tag. Ruler.
+    echo "<hr>Clouds are high in the air.</hr>"; # This is the horizontal rule tag. Ruler.
 
-    echo "<br> The sun shines on them to show the linings. </br>"; # This is the break tag, it makes a single line break.
+    echo "<br>The sun shines on them to show the linings.</br>"; # This is the break tag, it makes a single line break.
 
-    echo "<em> So it is not the cloud that gives the linings[?] </em>"; # This is the emphasis tag, typically rendered in italics, and screen readers will pronounce the words with emphasis, using verbal stress.
+    echo "<em>So it is not the cloud that gives the linings[?]</em>"; # This is the emphasis tag, typically rendered in italics, and screen readers will pronounce the words with emphasis, using verbal stress.
 
-    echo "<i> But it seems to be the sun? </i>"; # This is the italics tag. Italics.
+    echo "<i>But it seems to be the sun?</i>"; # This is the italics tag. Italics.
 
-    echo "<strong> Bro, I do not know what I am saying, am just rambling, dude. </strong>"; # This is the strong tag.
+    echo "<strong>Bro, I do not know what I am saying, am just rambling, dude.</strong>"; # This is the strong tag.
 
-    echo "<b> And I have now finally reached all the tags that I needed to test! </b>"; # This is the bold tag. It is bold.
+    echo "<b>And I have now finally reached all the tags that I needed to test!</b>"; # This is the bold tag. It is bold.
 
-    echo "<u> Noooo, it seems that there is one more tag 😒</u>"; # This is the underline tag. It underlines.
+    echo "<u>Nooo, it seems that there is one more tag 😒</u>"; # This is the underline tag. It underlines.
+
+    echo "<a href='page2.php'> Go to page 2</a>";
+
+    echo "<img src='vinci_library.png'>";
+
+    echo "<table>";
+
+        echo "<tr>";
+            echo "<td>Hello</td>";
+            echo "<td>Hi3</td>";
+
+        echo "</tr>";
+
+    echo "</table>";
 
     echo "</body>";
 
---
+++ May_26/HTML_Basics/version_1/page2.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<!DOCTYPE html>";
+
+echo "<html>";
+
+echo "<head>";
+echo "<title>Page 2: Lemon Juice</title>";
+echo "</head>";
+
+echo "<img src='lemonjuice.jpg'>";
+
+
+echo "</html>";
+?>
