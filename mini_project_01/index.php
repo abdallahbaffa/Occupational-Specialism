866318de046f9742f3672c356b89de645395ae2d
Abdallah.Baffa <abdallah92713@gmail.com>
2025-09-09 14:46:26 +0100

    Leaving now, will work at home, BETTER remember to update project when I get there and COMMIT AND PUSH at home before coming back to college.

--- mini_project_01/index.php
+++ mini_project_01/index.php
@@ -17,7 +17,25 @@ echo "<head><title>Abdallah's Mini Project #01 ~ The First Page: Colors! Interes
     background-position: center;}";
     echo "</style>";
 
-    echo "<h1 'center'>The First Page: Colors! Interesting facts about them.</h1>";
+    echo "<style>";
+    echo "#firsttitle {
+    border: 2px solid black;
+    padding: 10px;
+    background: url('images/second_title.jpg');
+    background-repeat: no-repeat;
+    background-size: auto;}";
+    echo "</style>";
+
+
+    echo "<div id='firsttitle'>";
+    echo "<h1 style='text-align: center'>The First Page: Colors! Interesting facts about them.</h1>";
+    echo "</div>";
+
+    echo "<h3 style='font-size: 30px'>Did you know?</h3>";
+    echo "<p style='font-family: 'Maiandra GD' Some people have a phobia of color? It is called Chromophobia</p>";
+    echo "<p> Color has a big impact on a first impression. About 62-90%! CAN RELATE.🔵";
+    echo "<p> People with Synesthesia might see different colors (if the type is Chromesthesia) when hearing different sounds.";
+
 
 echo "</body>";
 
