dde7112557004f2b522500e6d777d2645500fd06
Abdallah.Baffa <abdallah92713@gmail.com>
2025-09-09 12:24:55 +0100

    Hello :]
    Having MUCH trouble with background images, but I think now I got some of a hang on it... hopefully.

--- mini_project_01/index.php
+++ mini_project_01/index.php
@@ -4,19 +4,20 @@ echo "<!DOCTYPE html>";
 
 echo "<html>";
 
-    echo "<head><title>Abdallah's Mini Project #01 ~ The First Page: Colors! Interesting facts about them.</title>";
+echo "<head><title>Abdallah's Mini Project #01 ~ The First Page: Colors! Interesting facts about them.</title>";
     echo "<link rel='stylesheet' href='css/styles.css'></head>";
 
+    echo "<body>";
+
     echo "<style>";
-    echo "<body {
+    echo "body { background-image: url('images/vinci_library.png');
     background-repeat: no-repeat;
+    background-size: cover;
     background-attachment: fixed;
-    background-size: 100% 100%;
-}>";
+    background-position: center;}";
     echo "</style>";
-    echo "<img src='images/vinci_library.png'>";
 
-
+    echo "<h1 'center'>The First Page: Colors! Interesting facts about them.</h1>";
 
 echo "</body>";
 
