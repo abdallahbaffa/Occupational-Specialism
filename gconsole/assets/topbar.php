76dd448081277faba400461918a41960e9a80f7a
Abdallah.Baffa <abdallah92713@gmail.com>
2025-09-25 13:11:02 +0100

    Getting somewhere with gconsole, just on the last page for console registeration.

--- gconsole/assets/topbar.php
+++ gconsole/assets/topbar.php
@@ -2,7 +2,7 @@
 /*This is for the topbar.*/
 
 echo '<div class="topbar">';
-echo '<img src="images/index-icon.png" alt="Index Console" class="logo">';
+echo '<div class="topbar">';
 echo '<h1>Games Console</h1>';
 echo '</div>';
 
